3fbc8043ad46073dddebf983fd3e95e626d2d644
Isaiah Keller <isaiah.keller@gmail.com>
2022-08-22 20:52:03 -0700

    finishing for the night.  currently stuck on getting form to display and passing the form data back from the component.  I need to go back and finish horned beasts before I tackle this.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # City Explorer
 
 **Author**: Isaiah Keller
-**Version**: 1.0.1
+**Version**: 1.0.2
 
 ## Overview
 <!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
+import React, { Component } from 'react';
+import SearchForm from './components/SearchForm';
+import './App.css'
+import axios from 'axios';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      location: '',
+      mapData: {},
+      error: false,
+      errorMessage: ''
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  handleGetData = async (e) => {
+    e.preventDefault();
+    try {
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.location}&format=json`
+
+      let cityData = await axios.get(url);
+
+      this.setState({
+        mapData: cityData.data.results[0],
+        error: false
+      })
+
+    } catch (error) {
+      console.alert(error)
+      this.setState({
+        error: true,
+        errorMessage: `Error Occured: ${error.message}`
+      })
+    }
+  }
+
+  onChange = (e) => {
+    e.preventDefault();
+    this.setState({ text: e })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ location: this.state.text })
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <SearchForm changeHandler={this.onChange} submitHandler={this.onSubmit} />
+        {
+          this.state.error ?
+            <p>{this.state.errorMessage}</p> :
+            <>
+              <h3>{this.state.mapData.display_name}</h3>
+              <h3>{this.state.mapData.latitude}</h3>
+              <h3>{this.state.mapData.longitude}</h3>
+            </>
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
